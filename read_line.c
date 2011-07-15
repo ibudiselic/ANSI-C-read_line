471cf0b864a8fc90a3b4186b6710a82343d40de9
Ivan Budiselic <ivan.budiselic@gmail.com>
2011-07-15 23:56:57 +0200

    Fixed weird formatting error.

--- read_line.c
+++ read_line.c
@@ -7,34 +7,34 @@
 #define ALLOC_INCREMENT 1024 
 
 int read_line(char **s, FILE *fp) {
-	char *t=NULL, *p=NULL;
-	int c; /* character buffer */
-	int len = 0; /* current number of elements read and stored */
-	int allocated = 0; /* number of allocated bytes in p */
+    char *t=NULL, *p=NULL;
+    int c; /* character buffer */
+    int len = 0; /* current number of elements read and stored */
+    int allocated = 0; /* number of allocated bytes in p */
 
-	p = malloc(ALLOC_INCREMENT);
-	if (p == NULL) {
-		return -1;
+    p = malloc(ALLOC_INCREMENT);
+    if (p == NULL) {
+        return -1;
     }
-	allocated = ALLOC_INCREMENT;
+    allocated = ALLOC_INCREMENT;
 
     /* get next character from file */
-	while ((c = fgetc(fp))!=EOF && c!='\n') {
-		if (len == allocated) { /* buffer is full, must extend */
-			t = realloc(p, allocated + ALLOC_INCREMENT);
-			if (t == NULL) {
-				free(p);
-				return -1;
-			}
+    while ((c = fgetc(fp))!=EOF && c!='\n') {
+        if (len == allocated) { /* buffer is full, must extend */
+            t = realloc(p, allocated + ALLOC_INCREMENT);
+            if (t == NULL) {
+                free(p);
+                return -1;
+            }
 
-			p = t;
-			allocated += ALLOC_INCREMENT;
-		}
-		p[len++] = (char)c;
-	}
-	if (len == 0) {
+            p = t;
+            allocated += ALLOC_INCREMENT;
+        }
+        p[len++] = (char)c;
+    }
+    if (len == 0) {
         if (c == EOF) { /* empty file or read error */
-	        free(p);
+            free(p);
             return -1;
         } else { /* empty line */
             assert(c == '\n');
@@ -42,19 +42,19 @@ int read_line(char **s, FILE *fp) {
             *s = p;
             return 0;
         }
-	}
+    }
 
-	if (len+1 != allocated) { /* no room for '\0' */
-		t = realloc(p, len+1);
-		if (t == NULL) {
-			free(p);
-			return -1;
-		}
-		p = t;
-	}
+    if (len+1 != allocated) { /* no room for '\0' */
+        t = realloc(p, len+1);
+        if (t == NULL) {
+            free(p);
+            return -1;
+        }
+        p = t;
+    }
 
-	p[len] = '\0';
-	*s = p;
+    p[len] = '\0';
+    *s = p;
 
-	return len;
+    return len;
 }
